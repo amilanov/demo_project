6e7abb92e76e84744eda0752b8bbb1125ecd84e6
Aldin Milanovic <aldin@atlantbh.com>
2017-09-26 21:22:48 +0200

    Init

---
+++ demo_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo_project
+//
+//  Created by Aldin Milanovic on 26/09/2017.
+//  Copyright © 2017 Aldin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
